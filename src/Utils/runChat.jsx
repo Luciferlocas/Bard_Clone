c8bad8a22d4ac1446d605f289fba08ec16aeb399
Luciferlocas <rohitsomvanshi2003@gmail.com>
2024-07-03 21:29:17 +0530

    added key

--- src/Utils/runChat.jsx
+++ src/Utils/runChat.jsx
@@ -5,7 +5,7 @@ import {
 } from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-1.0-pro";
-const API_KEY = process.env.API_KEY;
+const API_KEY = "AIzaSyCaj479SQvJnZQDhiPxrFVeljge-O1i4sQ";
 
 async function runChat(prop) {
   const genAI = new GoogleGenerativeAI(API_KEY);
